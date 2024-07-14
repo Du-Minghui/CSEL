724dd03a8dec75bd6df40b3096204f7ed61debc2
Du Minghui <89332677+Du-Minghui@users.noreply.github.com>
2024-07-14 13:12:59 +0800

    Create nrps_bigscape.sh

---
+++ Shell scripts/nrps_bigscape.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while read line
+do
+python3 /home/dmh/miniconda3/envs/bigscape/lib/python3.7/site-packages/BiG-SCAPE/bigscape.py \
+        -c 10 -i /home/dmh/bigscape/nrps/gbk/$line \
+        -o /home/dmh/bigscape/nrps/res/$line \
+        --pfam_dir /home/dmh/data/databases/pfam/34.0 \
+        --mix --no_classify 
+done < filename.txt
+
